c2f9131be32c3534af42a05cb04c1a797c03b660
Vadim Sikora <vadim.sikora@divio.ch>
2016-05-15 20:20:25 +0200

    more tests

--- tests/unit/utils.test.js
+++ tests/unit/utils.test.js
@@ -1,4 +1,9 @@
-import { noscript, setStorage, __RewireAPI__ as UtilsRewire } from '../../static/js/addons/utils';
+import {
+    noscript,
+    setStorage,
+    getStorage,
+    __RewireAPI__ as UtilsRewire
+} from '../../static/js/addons/utils';
 import $ from 'jquery';
 
 describe('noscript()', () => {
@@ -11,13 +16,51 @@ describe('noscript()', () => {
 });
 
 describe('setStorage()', () => {
-    it('should return false if no token passed');
-    it('should return false if no value passed');
-    it('should return false if no token and no value passed');
+    beforeEach(() => localStorage.clear());
+    afterEach(() => UtilsRewire.__ResetDependency__('isStorageSupported'));
+
+    it('should return false if no token passed', () => {
+        expect(setStorage(null, 'value')).toEqual(false);
+    });
+
+    it('should return false if no value passed', () => {
+        expect(setStorage('token')).toEqual(false);
+    });
+
+    it('should return false if no token and no value passed', () => {
+        expect(setStorage()).toEqual(false);
+    });
+
     it('should return false if localstorage is not available', () => {
         UtilsRewire.__Rewire__('isStorageSupported', false);
         expect(setStorage('token', 'value')).toEqual(false);
     });
 
-    it('should put something in localstorage if it is available');
+    it('should put something in localstorage if it is available', () => {
+        expect(setStorage('token', 'value')).toEqual('value');
+        expect(localStorage.getItem('token')).toEqual('value');
+    });
+});
+
+describe('getStorage()', () => {
+    beforeEach(() => localStorage.clear());
+    afterEach(() => UtilsRewire.__ResetDependency__('isStorageSupported'));
+
+    it('should return false if no token passed', () => {
+        expect(getStorage()).toEqual(false);
+    });
+
+    it('should return false if localstorage is not available', () => {
+        UtilsRewire.__Rewire__('isStorageSupported', false);
+        expect(getStorage('token')).toEqual(false);
+    });
+
+    it('returns null if item is not in localstorage', () => {
+        expect(getStorage('token')).toEqual(null);
+    });
+
+    it('should get something from localstorage if it is available', () => {
+        setStorage('token', 'value');
+        expect(localStorage.getItem('token')).toEqual('value');
+    });
 });
